4a9398ec281c2f85a30770537ad01c15d544b36b
Hisham Khan <khanhisham25@gmail.com>
2023-05-22 14:46:45 +0530

    add prod url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const PASSWORD_REGEX = /^[a-zA-Z@_0-9]{8,30}$/i;
 export const USERNAME_REGEX = /^[a-zA-Z0-9]{3,30}$/i;
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3001/api/v1";
+export const BASE_URL = "https://mini-twitter-m7q8.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:3001/api/v1";
